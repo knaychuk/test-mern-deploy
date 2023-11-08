52ab17a85589452b2cd73518b335d254478f7bc0
Kate <knaychuk@gmail.com>
2023-11-08 04:39:57 -0800

    connect to mongo

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
 const itemRoutes = require('./routes/itemRoutes')
 
-const express = require('express');
+//express app
 const app = express();
 
 //middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
-
+//routes
 app.use("/api/items", itemRoutes)
 
-app.listen(5000, console.log("listening on port 5000"))
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+
+    app.listen(process.env.PORT, console.log("listening on port 5000"))
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
